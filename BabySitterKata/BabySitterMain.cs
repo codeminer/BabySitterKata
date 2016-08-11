cd09a3c2dcd19c90140ac10b63808ea0fc9d3f4a
yadelani <yussuf.kazade@gmail.com>
2016-08-10 20:43:14 -0500

    Tried Main method

--- BabySitterKata/BabySitterMain.cs
+++ BabySitterKata/BabySitterMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BabySitter.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace BabySitterKata
     {
         static void Main(string[] args)
         {
+            Sitter babysitter = new Sitter();
+            Console.WriteLine("The pay is " + babysitter.getPayCharge("11 PM", "5 PM", "4 AM"));
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
